da2d0cefe48a98bb7566d64f93d2e115a4fa9384
Moran <method.even@gmail.com>
2022-08-25 15:16:55 -0700

    Add User model authentication Sequelize scopes and methods

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,52 +1,98 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Validator } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    toSafeObject() {
+      const { id, username, email } = this;
+      return { id, username, email };
+    }
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
+    static async login({ credential, password }) {
+      const { Op } = require("sequelize");
+      const user = await User.scope("loginUser").findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential,
+          },
+        },
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope("currentUser").findByPk(user.id);
+      }
+    }
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword,
+      });
+      return await User.scope("currentUser").findByPk(user.id);
+    }
+
+
+    
     static associate(models) {
       // define association here
     }
   }
-  User.init({
-       
+  User.init(
+    {
       username: {
-        type:DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
         unique: true,
         validate: {
-          len: [4,30],
+          len: [4, 30],
           isNoEmail(value) {
-            if(validator.isEmail(value)) {
-              throw new Error("Cannot be email.")
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be email.");
             }
-          }
-        }
+          },
+        },
       },
 
       email: {
-       type: DataTypes.STRING,
-       allowNull: false,
-       unique: true,
-       validate: {
-        isEmail: true,
-        len: [3,256]
-       }
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+          len: [3, 256],
+        },
       },
       hashedPassword: {
         type: DataTypes.STRING.BINARY,
         allowNull: false,
         validate: {
-          len: [60,60]
-        }
+          len: [60, 60],
+        },
+      },
     },
-  },
     {
       sequelize,
       modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["email", "hashedPassword", "createdAt", "updatedAt"],
+        },
+      },
+      scopes: {
+        currentUser: {
+          attributes: { exclude: ["hashedPassword"] },
+        },
+        loginUser: {
+          attributes: {},
+        },
+        
+        
+      },
     }
   );
   return User;
