701dd8e366a22742969b4def956efd135c1d67df
Moran <method.even@gmail.com>
2022-08-26 10:43:42 -0700

    login function works

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 const { requireAuth } = require("../../utils/auth.js");
 const { setTokenCookie } = require("../../utils/auth.js");
@@ -10,6 +12,19 @@ router.use(restoreUser);
 router.get("/restore-user", (req, res) => {
   return res.json(req.user);
 });
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
 
 //POST test
 router.post("/test", function (req, res) {
