15fef587e5eed640ba6791d4415d912ef597701b
Moran <method.even@gmail.com>
2022-08-26 15:38:27 -0700

    sign ups validation password atleast 6 charecters

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,7 +4,21 @@ const router = express.Router();
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
-router.post("/", async (req, res, next) => {
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
+router.post("/", validateLogin, async (req, res, next) => {
   const { credential, password } = req.body;
 
   const user = await User.login({ credential, password });
