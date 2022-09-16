8ca4ed30be613c97791e8194c797fdd840ddffe7
Moran <method.even@gmail.com>
2022-09-16 07:57:38 -0700

    first request works

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { restoreCSRF, csrfFetch } from "./csrf";
 
 const rootReducer = combineReducers({
   // add reducer functions here
@@ -20,4 +21,12 @@ const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
 };
 
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 export default configureStore;
