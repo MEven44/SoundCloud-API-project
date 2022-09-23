7993c27749340b02960c721b64566917e9a61d26
Moran <method.even@gmail.com>
2022-09-23 11:13:11 -0700

    new seeders

--- backend/db/seeders/20220825212406-albums.js
+++ backend/db/seeders/20220825212406-albums.js
@@ -3,18 +3,20 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     
-    await queryInterface.bulkInsert('Albums', [
+    await queryInterface.bulkInsert("Albums", [
       {
         userId: 1,
-        title: "raining fella",
+        title: "water songs",
         description: "fun disco gay song",
-        imageUrl: null,
+        imageUrl:
+          "https://www.diamonddotz.com/image/cache/catalog/products/main/dq9/dq9.013-400x400.jpg",
       },
       {
         userId: 2,
-        title: "9th symhony",
+        title: "fire songs",
         description: "amazing",
-        imageUrl: null,
+        imageUrl:
+          "https://c8.alamy.com/comp/2HW15H8/art-inspired-by-sample-of-cloth-old-kingdom-dynasty-6-ca-23532323-bc-from-egypt-memphite-region-saqqara-pyramid-of-king-pepi-i-mummy-chamber-linen-10-cm-square-classic-works-modernized-by-artotop-with-a-splash-of-modernity-shapes-color-and-value-eye-catching-visual-impact-on-art-emotions-through-freedom-of-artworks-in-a-contemporary-way-a-timeless-message-pursuing-a-wildly-creative-new-direction-artists-turning-to-the-digital-medium-and-creating-the-artotop-nft-2HW15H8.jpg",
       },
     ]);
   
--- backend/db/seeders/20220825212407-songs.js
+++ backend/db/seeders/20220825212407-songs.js
@@ -3,28 +3,26 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
  
-      await queryInterface.bulkInsert(
-        "Songs",
-        [
-          {
-            albumId: 1,
-            userId: 1,
-            title: "time to rest",
-            description: "original material",
-            url: "https://soundcloud.com/moran-even/now-i-can-rest",
-            imageUrl: null,
-          },
-          {
-            albumId: 2,
-            userId: 1,
-            title: "on the lake",
-            description: "original material",
-            url: 'https://soundcloud.com/moran-even/the-big-fish',
-            imageUrl: null,
-          },
-        ],
-        
-      );
+      await queryInterface.bulkInsert("Songs", [
+        {
+          albumId: 1,
+          userId: 1,
+          title: "fly",
+          description: "this bird can fly",
+          url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3",
+          imageUrl:
+            "https://image.made-in-china.com/2f0j00TRBGZsSMAApD/Beautiful-Cocks-Diamond-Painting-Square-5D-Gemstone-Art-Painting-on-Canvas-.jpg",
+        },
+        {
+          albumId: 2,
+          userId: 1,
+          title: "on the lake",
+          description: "original material",
+          url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3",
+          imageUrl:
+            "https://cdn.shopify.com/s/files/1/1486/9868/products/yangtze-store-large-square-silk-scarf-classic-painting-waterlilies-by-monet-szd215-13721857359937.jpg?v=1582757679",
+        },
+      ]);
     
   },
 
